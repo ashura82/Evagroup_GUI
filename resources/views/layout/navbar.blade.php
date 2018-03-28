41aa5d784ec5a4295804987e10e0ef60eafb7c51
Robin LAURENS <robin.laurens@oxylio.com>
2018-03-28 16:14:58 +0200

    responsive menu

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -8,5 +8,9 @@
     <a class="navbar-brand" href="/">EVAGROUP GUI</a>
 </div>
 <div id="navbar" class="navbar-collapse collapse">
-
+    <ul class="nav navbar-nav navbar-right hidden-sm hidden-md hidden-lg">
+        <li><a href="{!! route('index') !!}">Overview</a></li>
+        <li><a href="{!! route('websites.index') !!}">Websites</a></li>
+        <li><a href="{!! route('firewall.index') !!}">Firewall</a></li>
+    </ul>
 </div>
